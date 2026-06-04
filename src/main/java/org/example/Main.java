7c0b85b330fab6454e903d17217fd3452fa1ab61
Hamza Yusuf Aktitiz <hamzayaktitiz@outlook.de>
2026-06-04 10:50:33 +0200

    Erster Schritt: JDA connected to D_Bot.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,31 @@
 package org.example;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        String y = "OsuBotTest";
-        String name = "Hamza";
-        IO.println("Hello " + name +  " Osu! " + x );
-        IO.println(String.format("%s",y));
+        String token = System.getenv("DISCORD_TOKEN");
+
+        if (token == null){
+            IO.println("[ERROR]: No Discord token provided!");
+            return;
+        }
+
+        IO.println("Starte den osu! Bot");
+
+        try{
+            JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
+        }
+        catch(Exception error){
+            IO.println("[ERROR]: Something went wrong: " + error.getMessage());
+        }
+
+        IO.println("Bot wurde erfolgreich gestarted!");
+
+
+
+
+
     }
 }
