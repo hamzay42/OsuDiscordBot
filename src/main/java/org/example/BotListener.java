a9ca81445dc3cba00cf68fe908602c302d836908
Hamza Yusuf Aktitiz <hamzayaktitiz@outlook.de>
2026-06-07 23:25:58 +0200

    Created BotListener class. Bot can read the general chat and answer to a call!

---
+++ src/main/java/org/example/BotListener.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class BotListener extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+        if (event.getMessage().getContentRaw().startsWith("!ping")) {
+            event.getChannel().sendMessage("Pong!").queue();
+
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,31 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static void main(String[] args) {
-        String token = System.getenv("DISCORD_TOKEN");
+        String token = System.getenv("DISCORD_TOKEN"); //takes token from environment variable
 
-        if (token == null){
+        if (token == null) {
             IO.println("[ERROR]: No Discord token provided!");
             return;
         }
 
         IO.println("Starte den osu! Bot");
 
-        try{
-            JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
-        }
-        catch(Exception error){
+        try {
+            JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                    .addEventListeners(new BotListener())
+
+
+                    .build(); //connects to discord
+        } catch (Exception error) {
             IO.println("[ERROR]: Something went wrong: " + error.getMessage());
         }
 
         IO.println("Bot wurde erfolgreich gestarted!");
+
+
+
+
+
 
 
 
